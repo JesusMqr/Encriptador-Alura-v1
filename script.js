960b533eec788af3714e124da7884fde86176bc0
Jesus <jesusmaquera10@gmail.com>
2023-06-04 21:07:09 -0500

    Optimizacion de codigo JS

--- script.js
+++ script.js
@@ -1,67 +1,64 @@
+/*botones*/
+const btnEncriptar=document.getElementById("btn-encriptar");
+const btnDesencriptar=document.getElementById("btn-desencriptar");
+const btnCopiar=document.getElementById("btn-copiar");
+
+/*  TextArea */
+const txtArea=document.getElementById("texto");
+const txtResultado = document.getElementById("resultado");
+
+/* Elementos */
+const muneco = document.getElementById("munheco");
+const contenido = document.getElementById("contenido");
 
 function encriptar(){
-    let texto= document.getElementById("texto").value;
+    let texto= txtArea.value;
     texto=texto.toLowerCase();
+    
     texto = texto.replace(/e/g,'enter');
     texto = texto.replace(/i/g,'imes');
     texto = texto.replace(/a/g,'ai');
     texto = texto.replace(/o/g,'ober');
     texto = texto.replace(/u/g,'ufat');
 
-    let resultado = document.getElementById("resultado");
-    resultado.style.textAlign = "justify";
-    resultado.innerHTML = texto;
-    
-    mostrarBtn();
+    txtResultado.textContent = texto;
+    mostrarBtn();  
 }
 
 function desencriptar(){
-    let texto= document.getElementById("texto").value;
+    let texto= txtArea.value;
     texto=texto.toLowerCase();
+    
     texto = texto.replace(/enter/g,'e');
     texto = texto.replace(/imes/g,'i');
     texto = texto.replace(/ai/g,'a');
     texto = texto.replace(/ober/g,'or');
     texto = texto.replace(/ufat/g,'u');
 
-
-    let resultado = document.getElementById("resultado");
-    resultado.style.textAlign = "justify";
-    resultado.innerHTML = texto;
-    
-    mostrarBtn();
+    txtResultado.textContent = texto;
+    mostrarBtn(); 
 }
 
 function copiar(){
-    let mensaje = document.getElementById('resultado').textContent;
-    navigator.clipboard.writeText(mensaje);
+    navigator.clipboard.writeText(txtResultado.textContent);
+    limpiar();
     ocultarBtn();
-    reset();
-
 }   
 
+function limpiar(){
+    txtResultado.style.textAlign = "center";
+    resultado.innerHTML = "Ningún mensaje fue encontrado";
+    texto.value="";
+}
+
 function mostrarBtn(){
-    let btnCopiar = document.getElementById("btn-copiar");
-    let munheco = document.getElementById("munheco");
-    let contenido = document.getElementById("contenido");
     btnCopiar.style.display="block";
     contenido.style.display="none";
     munheco.style.display="none";
 }
 
 function ocultarBtn(){
-    let btnCopiar = document.getElementById("btn-copiar");
-    let munheco = document.getElementById("munheco");
-    let contenido = document.getElementById("contenido");
     btnCopiar.style.display="none";
     contenido.style.display="block";
     munheco.style.display="block";
-}
-
-function reset(){
-    let resultado = document.getElementById("resultado");
-    resultado.style.textAlign = "center";
-    resultado.innerHTML = "Ningún mensaje fue encontrado";
-    document.getElementById("texto").value="";
-
 }
